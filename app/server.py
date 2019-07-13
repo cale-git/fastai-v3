bbff08e999b5d853d3456c60a61c1ec0f16820b2
cale-git <52862182+cale-git@users.noreply.github.com>
2019-07-13 12:53:43 -0400

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://www.googleapis.com/drive/v3/files/1zw-_4M0DMa-Ei3KUGY7OicaMLh2dlNIQ?alt=media&key=AIzaSyAiB_ylknjY_qTg6J-3yqwOK5eO_M03IVA'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['bicycle', 'motorcycle', 'unicycle']
 path = Path(__file__).parent
 
 app = Starlette()
